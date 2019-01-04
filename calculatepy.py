e469961ac89770745f0020cc100a63e7bde42b03
PZ1Sharer05 <44546014+PZ1Sharer05@users.noreply.github.com>
2019-01-04 08:30:33 +0700

    Add files via upload

---
+++ calculatepy.py
@@ -0,0 +1,28 @@
+
+
+def calculateSum(nsone, nstwo):
+    print (nsone + nstwo)
+
+def calculateDifference(ndone, ndtwo):
+    print (ndone - ndtwo)
+
+def calculateMultiplication(nmone, nmtwo):
+    print (nmone * nmtwo)
+
+
+def calculateQuotient(nqone, nqtwo):
+    print (nqone / nqtwo)
+
+def calculateDivisionRemainder(nrone, nrtwo):
+    print (nrone % nrtwo)
+
+
+def calculatePowers(number, power):
+    print (number ** power)
+    
+    
+    
+    
+    
+
+
